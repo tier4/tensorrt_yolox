1d3eb9b1f296d77d344ee84a9b00aad0b682df70
Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>
2022-10-27 17:07:43 +0900

    feat: add support for plain YOLOX model
    feat: add ROS node argument to control thresholds
    
    feat: use CUDA handled host memory for data transfer efficiency

--- include/tensorrt_yolox/tensorrt_yolox.hpp
+++ include/tensorrt_yolox/tensorrt_yolox.hpp
@@ -15,6 +15,7 @@
 #ifndef TENSORRT_YOLOX__TENSORRT_YOLOX_HPP_
 #define TENSORRT_YOLOX__TENSORRT_YOLOX_HPP_
 
+#include "cuda_utils/cuda_unique_ptr.hpp"
 #include <cuda_utils/cuda_unique_ptr.hpp>
 #include <cuda_utils/stream_unique_ptr.hpp>
 #include <opencv2/opencv.hpp>
@@ -28,6 +29,7 @@
 namespace tensorrt_yolox
 {
 using cuda_utils::CudaUniquePtr;
+using cuda_utils::CudaUniquePtrHost;
 using cuda_utils::makeCudaStream;
 using cuda_utils::StreamUniquePtr;
 
@@ -44,11 +46,20 @@ struct Object
 using ObjectArray = std::vector<Object>;
 using ObjectArrays = std::vector<ObjectArray>;
 
+struct GridAndStride {
+  int grid0;
+  int grid1;
+  int stride;
+};
+
 class TrtYoloX
 {
 public:
   TrtYoloX(
     const std::string & model_path, const std::string & precision,
+    const int num_class=8,
+    const float score_threshold=0.3,
+    const float nms_threshold=0.7,
     const std::string & cache_dir = "",
     const tensorrt_common::BatchConfig & batch_config = {1, 1, 1},
     const size_t max_workspace_size = (1 << 30));
@@ -57,6 +68,33 @@ public:
 
 private:
   void preprocess(const std::vector<cv::Mat> & images);
+  bool feedforward(const std::vector<cv::Mat> &images, ObjectArrays &objects);
+  bool feedforwardAndDecode(const std::vector<cv::Mat> &images, ObjectArrays &objects);
+  void decodeOutputs(float *prob, ObjectArray &objects, float scale, cv::Size &img_size) const;
+  void generateGridsAndStride(const int target_w, const int target_h,
+                              std::vector<int> &strides,
+                              std::vector<GridAndStride> &grid_strides) const;
+  void generateYoloxProposals(std::vector<GridAndStride> grid_strides,
+                              float *feat_blob, float prob_threshold,
+                              ObjectArray &objects) const;
+  void qsortDescentInplace(ObjectArray &faceobjects, int left, int right) const;
+  inline void qsortDescentInplace(ObjectArray &objects) const
+  {
+    if (objects.empty()) {
+      return;
+    }
+    qsortDescentInplace(objects, 0, objects.size() - 1);
+  }
+  inline float intersectionArea(const Object &a, const Object &b) const
+  {
+    cv::Rect a_rect(a.x_offset, a.y_offset, a.width, a.height);
+    cv::Rect b_rect(b.x_offset, b.y_offset, b.width, b.height);
+    cv::Rect_<float> inter = a_rect & b_rect;
+    return inter.area();
+  }
+  void nmsSortedBboxes(const ObjectArray &faceobjects, std::vector<int> &picked,
+                       float nms_threshold) const;
+
   std::unique_ptr<tensorrt_common::TrtCommon> trt_common_;
 
   std::vector<float> input_h_;
@@ -66,10 +104,21 @@ private:
   CudaUniquePtr<float[]> out_scores_d_;
   CudaUniquePtr<int32_t[]> out_classes_d_;
 
+  bool needs_output_decode_;
+  size_t out_elem_num_;
+  size_t out_elem_num_per_batch_;
+  CudaUniquePtr<float[]> out_prob_d_;
+
   StreamUniquePtr stream_{makeCudaStream()};
 
   int32_t max_detections_;
   std::vector<float> scales_;
+
+  int num_class_;
+  float score_threshold_;
+  float nms_threshold_;
+
+  CudaUniquePtrHost<float[]> out_prob_h_;
 };
 
 }  // namespace tensorrt_yolox
